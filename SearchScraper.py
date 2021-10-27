b1798809d8df3a40d2d1a18ef78bef2a99324f5a
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2021-10-27 01:06:51 +0100

    Functions Added

--- SearchScraper.py
+++ SearchScraper.py
@@ -1,28 +1,48 @@
 from googlesearch import search
 from bs4 import BeautifulSoup as bs
 import requests
-import urllib
+import os
+
+import time
+
+start_time = time.time()
 
 
 # Get the links
 def ScrapeLink(query):
-    result = search(query, tld="com", num=2, stop=2, pause=2)
+    result = search(query, stop=40, pause=2)
+
     return result
 
 
 # Get the HTML from the link
 def getLinkData(link):
     response = requests.get(link)
+
     HTMLtxt = bs(response.text, 'html.parser')
-    return HTMLtxt, HTMLtxt.title.text
+
+    return HTMLtxt
 
 
-# Write the HTML to a file
-def writeToFile(title, data):
-    print(type(title))
-    filename = title + ".txt"
-    f = open(filename, "w+")
-    f.write(str(data.html.encode('utf8')))
+# Write the HTML to a file and put inside directory
+def writeToFile(data, index):
+    # Declaring the path
+    parentDir = "SearchResults/"
+    # dirName = "Link" + index
+    filename = "Link" + index + ".txt"
+
+    # fullPath = os.path.join(parentDir, dirName)
+
+    # Creating directory
+    # if not os.path.exists(fullPath):
+    # os.mkdir(fullPath)
+
+    # Creating the file
+    f = open(parentDir + filename, "w+")
+    try:
+        f.write(str(data.html.encode('utf8')))
+    except AttributeError:
+        print("Webpage Null")
 
 
 if __name__ == "__main__":
@@ -32,10 +52,15 @@ if __name__ == "__main__":
 
     # Get the links and store in result
     result = ScrapeLink(Keyword)
+    print(result)
+    print("Links found in --- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
 
     # Iterate through the links
-    for j in result:
-        fileTxt, t = getLinkData(j)
-        writeToFile(t, fileTxt)
+    i = 1
+    for links in result:
+        fileTxt = getLinkData(links)
+        writeToFile(fileTxt, str(i))
+        i += 1
 
-    print(t)
+    print("Stored in file in --- %s seconds ---" % (time.time() - start_time))
