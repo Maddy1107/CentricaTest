0fcbff53bb7e10b99b58cacb3f9eddf75edca1d6
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2021-10-27 21:37:10 +0100

    Added DockerFile

--- SearchScraper.py
+++ SearchScraper.py
@@ -2,8 +2,7 @@ from googlesearch import search
 from bs4 import BeautifulSoup as bs
 import requests
 import os
-
-import time
+import re
 
 
 # Get the links
@@ -14,61 +13,82 @@ def ScrapeLink(query, amount):
 
 
 # Get the HTML from the link
-def getLinkData(link):
+def getLinkHTMLData(link):
     response = requests.get(link)
 
     HTMLtxt = bs(response.text, 'html.parser')
 
-    return HTMLtxt
+    return HTMLtxt, HTMLtxt.title.text
 
 
-# Breaking the webpage to chunks for improving time complexity.
-def getChunks(link):
-    chunks = link.find_all('h2')
-    return chunks
+# --------------Code to minimize the run time by creating chunks---------------------
+# # Breaking the webpage to chunks for improving time complexity.
+# def getChunks(link):
+#     try:
+#         chunks = link.find_all('h2')
+#         return chunks
+#     except AttributeError:
+#         print("No H2 tags")
+
+
+def checkForInvalidName(name):
+    invalid_symbols = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
+    if invalid_symbols.search(name) is not None:
+        name = re.sub(invalid_symbols, '', name)
+    return name
 
 
 # Write the HTML to a file and put inside directory
-def writeToFile(data, index):
+def writeToFile(data, FileTitle):
     # Declaring the paths
-    parentDir = "SearchResults/"
-    # dirName = "Link" + index
-    filename = "Link" + index + ".txt"
+    parentDir = 'SearchResults/'
 
-    # fullPath = os.path.join(parentDir, dirName)
+    # dirName = checkForInvalidName(PageTitle)
 
-    # Creating directory
-    # if not os.path.exists(fullPath):
-    # os.mkdir(fullPath)
+    if FileTitle is not None:
+        filename = checkForInvalidName(FileTitle) + '.txt'
+    else:
+        filename = 'Link.txt'
+
+    fullPath = os.path.join(parentDir, filename)
 
     # Creating the file
-    f = open(parentDir + filename, "w+")
+    f = open(fullPath, "w+")
+
     try:
         f.write(str(data.html.encode('utf8')))
     except AttributeError:
         print("Webpage Null")
 
+    # --------------Code to create a directory when creating chunks---------------------
+    # Creating directory
+    # if not os.path.exists(fullPath):
+    # os.mkdir(fullPath)
+
 
 def readAndSaveLinksHTML(scrapedLinks):
     # Iterate through the links
-    i = 1
     for links in scrapedLinks:
-        start_time = time.time()
-
         # Get the full HTML file
-        fileTxt = getLinkData(links)
-        # Get chunks from the file by H2 tag
-        print(getChunks(fileTxt))
-        # Write to files
-        #writeToFile(fileTxt, str(i))
-        i += 1
+        fileHTMLTxt, FileTitle = getLinkHTMLData(links)
 
-        print("Stored in file in --- %s seconds ---" % (time.time() - start_time))
+        # Write to files
+        writeToFile(fileHTMLTxt, FileTitle)
+
+        # --------------Code to minimize the run time by creating chunks---------------------
+        # Get chunks from the file by H2 tag
+        # H2tags = getChunks(fileHTMLTxt)
+        # for i in H2tags:
+        #     FileTitle = i.get_text()
+        #     # Write to files
+        # writeToFile(fileHTMLTxt, FileTitle, PageTitle)
 
 
 if __name__ == "__main__":
     # Search Keyword
     Keyword = "how to data engineering"
+
     # Number of Links wanted
     number_of_links = 1
 
